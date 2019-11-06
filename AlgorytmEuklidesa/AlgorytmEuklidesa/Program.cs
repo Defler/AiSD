036e797fadee8ef3c2aaeca44aa9bfd29858afeb
Defler <48053668+Defler@users.noreply.github.com>
2019-11-06 16:10:48 +0100

    Add files via upload

---
+++ AlgorytmEuklidesa/AlgorytmEuklidesa/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AlgorytmEuklidesa
+{
+    class Program
+    {
+        static int Euklides(int a, int b)
+        {
+            int pom;
+            if (a % b == 0)
+                return b;
+            else
+            {
+                while (a % b != 0)
+                {
+                    pom = a;
+                    a = b;
+                    b = pom % b;
+                }
+                return b;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Podaj liczbe a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbe b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("NWD tych liczb wynosi: " + Euklides(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
