ca7bda8b4a0165658e89c8cde30adf22edfb9f3b
Defler <48053668+Defler@users.noreply.github.com>
2019-11-06 16:13:44 +0100

    Add files via upload

---
+++ Kolejka/Kolejka/IAiSD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kolejka
+{
+    interface IAiSD
+    {
+        int Length { get; }
+        void Push(int a);
+        void Pop();
+    }
+}
---
+++ Kolejka/Kolejka/KolejkaMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kolejka
+{
+    class SD : IAiSD
+    {
+        int[] dane;
+        public int Length => this.dane.Length;
+        public int this[int index]
+        {
+            get { return this.dane[index]; }
+            set { this.dane[index] = value; }
+        }
+
+        public SD()
+        {
+            this.dane = new int[0];
+        }
+
+        public void Pop()
+        {
+            int[] nowa = new int[this.dane.Length - 1];
+            for(int i = 1; i<this.dane.Length; i++)
+            {
+                nowa[i - 1] = this.dane[i];
+            }
+            this.dane = nowa;
+        }
+
+        public void Push(int a)
+        {
+            var nowa = this.Przepisz();
+            nowa[nowa.Length - 1] = a;
+            this.dane = nowa;
+        }
+
+        int[] Przepisz()
+        {
+            int[] nowa = new int[this.dane.Length + 1];
+            for (int i = 0; i < this.dane.Length; i++)
+            {
+                nowa[i] = this.dane[i];
+            }
+            return nowa;
+        }
+
+
+    }
+}
