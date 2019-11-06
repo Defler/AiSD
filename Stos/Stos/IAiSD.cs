e037143703aa4c40ccaff34eed84f5ece377fd75
Defler <48053668+Defler@users.noreply.github.com>
2019-11-06 16:18:07 +0100

    Add files via upload

---
+++ Stos/Stos/IAiSD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stos
+{
+    interface IAiSD
+    {
+        void push(int a);
+        void pop();
+        int Length { get; }
+
+    }
+}
---
+++ Stos/Stos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Stos
+{
+    class SD: IAiSD
+    {
+        int[] dane;
+        public int Length => this.dane.Length;
+        public int this[int index]
+        {
+            get { return this.dane[index]; }
+            set { this.dane[index] = value; }
+        }
+
+        public SD()
+        {
+            this.dane = new int[0];
+        }
+        public void push(int a)
+        {
+            var nowa = this.Przepisz();
+            nowa[nowa.Length - 1] = a;
+            this.dane = nowa;
+        }
+
+        public void pop()
+        {
+            int[] nowa = new int[this.dane.Length - 1];
+            for(int i = 0; i<nowa.Length; i++)
+            {
+                nowa[i] = this.dane[i];
+            }
+            this.dane = nowa;
+        }
+
+        int[] Przepisz()
+        {
+            int[] nowa = new int[this.dane.Length + 1];
+            for (int i = 0; i < this.dane.Length; i++)
+            {
+                nowa[i] = this.dane[i];
+            }
+            return nowa;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
