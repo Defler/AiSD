cd84054e9293caa5cb09c329bf918a02d255b8ea
Defler <48053668+Defler@users.noreply.github.com>
2019-11-06 16:12:02 +0100

    Add files via upload

---
+++ CiagFibonacciego/CiagFibonacciego/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CiagFibonacciego
+{
+    class Program
+    {
+        static int Fibo(int n)
+        {
+            int poprzednia = 1;
+            int zapoprzednia = 1;
+            int liczba = 0;
+            int pom;
+            if (n == 1)
+                liczba = poprzednia;
+            else if (n == 2)
+                liczba = zapoprzednia;
+            else
+            {
+                for (int i = 3; i <= n; i++)
+                {
+                    liczba = poprzednia + zapoprzednia;
+                    pom = poprzednia;
+                    poprzednia = liczba;
+                    zapoprzednia = pom;
+                }
+            }
+            return liczba;
+        }
+        static void Main(string[] args)
+        {
+            string wczytane;
+            int n;
+            Console.WriteLine("Podaj n: ");
+            wczytane = Console.ReadLine();
+            n = int.Parse(wczytane);
+            Console.WriteLine("N-ta liczba Fibo: " + Fibo(n));
+
+            Console.ReadKey();
+
+        }
+    }
+}
